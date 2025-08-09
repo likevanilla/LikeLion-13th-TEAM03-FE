d6f8039055c23f320d6178bd0282783a5f70f1c4
likevanilla <wjdgur27507@gmail.com>
2025-08-09 15:29:48 +0900

    [feat](#2) 상단 메뉴바 제작

---
+++ sambakja/src/components/CommercialAnalysisReport.jsx
@@ -0,0 +1,29 @@
+import "./CommercialAnalysisReport.css";
+
+export default function CommercialAnalysisReport() {
+  return (
+    <div>
+      <nav>
+        <div className="Page-info">상권 분석</div>
+        <ul className="Menu">
+          <li>
+            <a href="#">홈</a>
+          </li>
+          <li>
+            <a href="#">업종 추천</a>
+          </li>
+          <li>
+            <a href="#">정책 안내</a>
+          </li>
+          <li>
+            <a href="#">서비스소개</a>
+          </li>
+          <li>
+            <a href="#">문의하기</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+Í;
