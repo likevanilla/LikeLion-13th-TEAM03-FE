96178f11eba5ae0e4d416b0cadd7865d63993f05
JeongBeanHyun <kelly92108@gmail.com>
2025-08-12 18:37:58 +0900

    [fix](LikeLion-13th-SKHU#8) 버튼별 데이터 출력하도록 수정

--- sambakja/src/pages/PolicyGuidePage.jsx
+++ sambakja/src/pages/PolicyGuidePage.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import HomeHeader from "../components/HomeHeader";
+import { useState } from "react";
 
 const Category = styled.div`
   background-color: #e7ffc1;
@@ -39,20 +40,87 @@ const Li = styled.div`
 `;
 
 export default function PolicyGuidePage() {
+  const end = {
+    전체: "/gu",
+    "자금 지원": "/gu/b",
+    "점포 지원": "/gu/st",
+    "교육 지원": "/gu/ed",
+  };
+
+  //우선 목업데이터로 채워둠.
+  const mock = {
+    "/gu": [
+      {
+        id: 1,
+        title: "[서울시] 자영업자 홍보 모집",
+        url: "https://...",
+        date: "2025-07-04",
+      },
+      {
+        id: 2,
+        title: "[서울시] 창업 안내",
+        url: "https://...",
+        date: "2025-07-04",
+      },
+    ],
+    "/gu/b": [
+      {
+        id: 3,
+        title: "[서울시] 대출 안내",
+        url: "https://...",
+        date: "2025-07-21",
+      },
+    ],
+    "/gu/st": [
+      {
+        id: 4,
+        title: "[서울시] 임대료 지원 안내",
+        url: "https://...",
+        date: "2025-08-02",
+      },
+    ],
+    "/gu/ed": [
+      {
+        id: 5,
+        title: "[서울시] 창업 교육 모집 안내",
+        url: "https://...",
+        date: "2025-08-01",
+      },
+    ],
+  };
+
+  const [active, setActive] = useState("전체");
+  const url = end[active];
+  const list = mock?.[url] ?? [];
+
   return (
     <div>
       <HomeHeader />
       <Category>
-        <Btn>전체</Btn>
-        <Btn>자금 지원</Btn>
-        <Btn>점포 지원</Btn>
-        <Btn>교육 지원</Btn>
+        {Object.keys(end).map((ca) => (
+          <Btn key={ca} onClick={() => setActive(ca)}>
+            {ca}
+          </Btn>
+        ))}
       </Category>
       <List>
-        <Li>
-          <span>[서울시] 서울시 자영업자 대상 홍보 모집</span>
-          <span>Jul 24, 2025</span>
-        </Li>
+        {list.map((item) => (
+          <Li key={item.id}>
+            <a
+              href={item.url}
+              style={{ color: "black", textDecoration: "none" }}
+            >
+              {item.title}
+            </a>
+            <span>
+              {new Date(item.date).toLocaleDateString("en-US", {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+              })}
+            </span>
+          </Li>
+        ))}
       </List>
     </div>
   );
