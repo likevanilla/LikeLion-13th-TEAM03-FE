fc95651709224364c22383cd4d536a504effbe3e
likevanilla <wjdgur27507@gmail.com>
2025-08-13 15:54:45 +0900

    [chore] 라우팅 설정

--- sambakja/src/App.jsx
+++ sambakja/src/App.jsx
@@ -1,7 +1,20 @@
-import CommercialAnalysisReport from "./components/commercialAnalysisReport";
+import { Routes, Route, Navigate } from "react-router-dom";
+import CommercialAnalysisReport from "./components/CommercialAnalysisReport";
+import IndustryRecommendationReport from "./components/IndustryRecommendationReport";
+import IndustryRecommendationQuestion from "./components/IndustryRecommendationQuestion";
+import Inquiry from "./components/Inquiry";
 
-function App() {
-  return <CommercialAnalysisReport />;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<IndustryRecommendationQuestion />} />
+      <Route
+        path="/industry-report"
+        element={<IndustryRecommendationReport />}
+      />
+      <Route path="/commercial-report" element={<CommercialAnalysisReport />} />
+      <Route path="/inquiry" element={<Inquiry />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
-
-export default App;
