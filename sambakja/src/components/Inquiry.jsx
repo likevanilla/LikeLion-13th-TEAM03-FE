f7915af669c7bfbcfb038ea97aa62a01a040376f
JeongBeanHyun <kelly92108@gmail.com>
2025-08-20 01:17:21 +0900

    [feat](LikeLion-13th-SKHU#27) api 연동 및 코드 수정 및 모달창 추가

--- sambakja/src/components/Inquiry.jsx
+++ sambakja/src/components/Inquiry.jsx
@@ -1,18 +1,78 @@
 import "./Inquiry.css";
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import HomeHeader from "./HomeHeader";
+import { api } from "../apis/api";
 
 export default function Inquiry() {
+  const [name, setName] = useState("");
+  const [phoneNum, setPhoneNum] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const [emailDomain, setEmailDomain] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [ok, setOk] = useState(false);
 
   const handleEmailDomainChange = (e) => {
     const selectedDomain = e.target.value;
     setEmailDomain(selectedDomain);
   };
 
+  async function postInquiry() {
+    try {
+      const res = await api.post("/api/inquiry", {
+        name: name.trim(),
+        phone: phoneNum.trim(),
+        email: `${email.trim()}@${emailDomain.trim()}`,
+        message: message.trim(),
+      });
+
+      console.log(res);
+
+      setOk(true);
+      setName("");
+      setPhoneNum("");
+      setEmail("");
+      setEmailDomain("");
+      setMessage("");
+      console.log("데이터 전송 성공");
+    } catch (e) {
+      setError(true);
+      console.log("데이터 전송 실패");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postInquiry();
+  };
+
+  function Modal() {
+    return (
+      <div className="simple-modal-backdrop" onClick={() => setOk(false)}>
+        <div className="simple-modal">
+          <p>
+            문의가 정상적으로 접수되었습니다.
+            <br />
+            빠른 시일 내에 답변드리겠습니다.
+          </p>
+          <button
+            type="button"
+            className="simple-modal-button"
+            onClick={() => setOk(false)}
+          >
+            확인
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="Inquiry-container">
+      {ok && <Modal />}
       <HomeHeader pageInfo="문의하기" />
       <header>
         <div>궁금하신 사항은 아래 양식을 통해 문의해 주시기 바랍니다.</div>
@@ -21,10 +81,16 @@ export default function Inquiry() {
       </header>
       <div className="Inquiry-form-container">
         <div className="Inquiry-form">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label className="form-label">이름</label>
-              <input type="text" className="form-input" required />
+              <input
+                type="text"
+                className="form-input"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
             </div>
             <div className="form-group">
               <label className="form-label">연락처</label>
@@ -32,6 +98,8 @@ export default function Inquiry() {
                 type="text"
                 className="form-input"
                 placeholder="예: 010-1234-5678"
+                value={phoneNum}
+                onChange={(e) => setPhoneNum(e.target.value)}
                 required
               />
             </div>
@@ -41,6 +109,8 @@ export default function Inquiry() {
                 <input
                   type="text"
                   className="form-input email-input"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                 />
                 <span className="email-at">@</span>
@@ -64,8 +134,15 @@ export default function Inquiry() {
             </div>
             <div className="form-group">
               <label className="form-label">문의 내용</label>
-              <textarea className="form-textarea" rows="6" required />
+              <textarea
+                className="form-textarea"
+                rows="6"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              />
             </div>
+
             <div className="button-container">
               <button type="submit" className="submit-button">
                 문의하기
