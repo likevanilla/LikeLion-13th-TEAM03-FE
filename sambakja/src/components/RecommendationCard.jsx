efeb72af6fe5ca797831664c59bee569e73199b3
JeongBeanHyun <kelly92108@gmail.com>
2025-08-25 22:40:19 +0900

    [feat](#43) LLM평가 추가

--- sambakja/src/components/RecommendationCard.jsx
+++ sambakja/src/components/RecommendationCard.jsx
@@ -1,6 +1,13 @@
 import "/src/style/IndustryRecommendationReport.css";
 
-const ORDER = ["유동인구", "직장인구", "연령층", "임대료", "상권특징"];
+const ORDER = [
+  "유동인구",
+  "직장인구",
+  "연령층",
+  "임대료",
+  "상권특징",
+  "LLM평가",
+];
 
 export default function RecommendationCard({ region = "", reason = {} }) {
   const rows = ORDER.map((key) => ({ key, value: reason?.[key] })).filter(
